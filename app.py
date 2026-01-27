cd1d8864faba0e11c1869f38bf76db211704bdbe
Rohit Jana <140490171+Rohitjana9098@users.noreply.github.com>
2026-01-28 00:27:25 +0530

    Update app.py
    git add app.py
    git commit -m "Fix: use HeartDisease as target column"
    git push

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ def load_data():
 
 df = load_data()
 
-X = df.drop("target", axis=1)
-y = df["target"]
+# 🔴 CHANGED HERE
+X = df.drop("HeartDisease", axis=1)
+y = df["HeartDisease"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
